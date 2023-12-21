bda2e0a2c8181d0f418e37bdca48df99f8c73aed
DianaEri <153087653+DianaEri@users.noreply.github.com>
2023-12-21 16:35:45 +0000

    Ändringar
    försöker minska mängd och storlek på footer så den inte täcker en hel mobilruta.

--- script.js
+++ script.js
@@ -30,3 +30,14 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
 }
 /* Slidern slutar här */
+
+const h2 = document.querySelector('.second_h2')
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
